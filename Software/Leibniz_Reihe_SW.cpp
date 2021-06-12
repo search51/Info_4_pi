72071c0fe1aacd9c9b467d45ad8804e8e64ca189
Johannes Ruffer <johannes.ruffer@gmx.de>
2021-06-12 14:26:17 +0200

    neue SW

--- Software/Leibniz_Reihe_SW.cpp
+++ Software/Leibniz_Reihe_SW.cpp
@@ -8,7 +8,7 @@
 
 long double calc_PI(int step_Size, int start_Point) {
         long double sum = 0;
-        for (int k = start_Point; k <= 3000000; k=k+step_Size)
+        for (int k = start_Point; k <= 300000000; k += step_Size)
         {
                 sum += pow(-1, k) / (2 * k + 1);
         }
@@ -16,9 +16,7 @@ long double calc_PI(int step_Size, int start_Point) {
 }
 
 int main (int argc, char** argv)
-{
-		std::cout << "Programm ist gestartet" << std::endl;
-		
+{		
         int size, rank, length;
 		long double sum = 0;
 
@@ -39,35 +37,35 @@ int main (int argc, char** argv)
         if (rank!=0)
         {
 			long double help_sum = 0;
-
-			std::cout << std::endl << "##### SLAVE " << rank << " #####" << std::endl;
 			help_sum = calc_PI(size-1, rank-1);
 
-			std::cout << "Ergebnis der Berechnung: " << help_sum;
+			//std::cout << "Ergebnis der Berechnung: " << help_sum;
 			
 			//MPI_Send( &length,sizeof(length) , MPI_INT, 0, 0, MPI_COMM_WORLD);
 			MPI_Send(&help_sum, 1 , MPI_LONG_DOUBLE, 0, 1, MPI_COMM_WORLD);
+			std::cout << std::endl << "##### SLAVE " << rank << " ##### Ergebnis: " << std::setprecision(5) << help_sum << std::endl;
         }
 
 		// master
 		if (rank==0)
 		{
 			long double help_sum = 0;
-			std::cout << std::endl << "##### MASTER ##### \n" << "Networksize: " << size << std::endl;
+			std::cout << std::endl << "##### MASTER #####" << std::endl;
 			
 			if (size==1)
 			{
-				//sum = calc_PI(1,0); //calc_PI(int step_Size, int start_Point)
+				sum = calc_PI(1,0); //calc_PI(int step_Size, int start_Point)
 			}
-			
-			for(int i = 1; i < size; i++)
-			{
-				//MPI_Recv(&length, 50, MPI_LONG_DOUBLE, 1, 1, MPI_COMM_WORLD, &status);
-				MPI_Recv(&help_sum, 1, MPI_LONG_DOUBLE, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
-				// Ausgabe der empfangenen Daten
-				std::cout << "Received Message Number " << i << ": " << help_sum << std::endl;
-				// Addition der Hilfssumme zur Hauptsumme
-				sum += help_sum;
+			else {
+				for (int i = 1; i < size; i++)
+				{
+					//MPI_Recv(&length, 50, MPI_LONG_DOUBLE, 1, 1, MPI_COMM_WORLD, &status);
+					MPI_Recv(&help_sum, 1, MPI_LONG_DOUBLE, i, 1, MPI_COMM_WORLD, &status);
+					// Ausgabe der empfangenen Daten
+					std::cout << "Received Message Number " << i << ": " << help_sum << std::endl;
+					// Addition der Hilfssumme zur Hauptsumme
+					sum += help_sum;
+				}
 			}
 
 			// sum into pi as result
@@ -75,12 +73,16 @@ int main (int argc, char** argv)
 	
 			// output
 			std::cout << std::endl << "PI: " << std::setprecision(50) << pi << std::endl;
+			// Berechnung und Ausgabe der Laufzeit
+			time += clock() - tstart;
+			time = time / CLOCKS_PER_SEC;
+			std::cout << "Laufzeit: " << std::setprecision(4) << time << std::endl;
 		}
 
 		// Berechnung und Ausgabe der Laufzeit
-		time += clock() - tstart;
-		time = time / CLOCKS_PER_SEC;
-		std::cout << std::endl << "Laufzeit: " << time;
+		//time += clock() - tstart;
+		//time = time / CLOCKS_PER_SEC;
+		//std::cout << std::endl << "Laufzeit: " << time << std::endl;
 
         //std::cout << "Run-Time: " << time << std::endl;
 
